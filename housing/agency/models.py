c58257276783391d4f52435865aeec322f672da5
skafis <franciskipchumba5@gmail.com>
2016-04-26 08:59:18 +0300

    add my models

--- housing/agency/models.py
+++ housing/agency/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Houses(models.Model):
+    picture = models.FileField(null=True, blank=True)
+    name = models.CharField(max_length=140)
+    location = models.CharField(max_length=200)
+    price =  models.DecimalField(max_digits=6, decimal_places=2)
+    bedrooms = models.IntegerField()
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
