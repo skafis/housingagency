0f43aad087d3517976423ddc148ad466b1cedc5b
skafis <franciskipchumba5@gmail.com>
2016-04-26 09:31:03 +0300

    register and change settings

--- housing/agency/admin.py
+++ housing/agency/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from  agency.models import Houses
 
 # Register your models here.
+admin.site.register(Houses)
--- housing/agency/models.py
+++ housing/agency/models.py
@@ -10,6 +10,7 @@ class Houses(models.Model):
     price =  models.DecimalField(max_digits=6, decimal_places=2)
     bedrooms = models.IntegerField()
     description = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __unicode__(self):
         return self.name
