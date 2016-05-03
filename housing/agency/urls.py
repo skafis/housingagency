13675208c1f4f7f5cf5bedd13771c82e4282e63b
skafis <franciskipchumba5@gmail.com>
2016-05-03 09:04:29 +0300

    include static filies

--- housing/agency/urls.py
+++ housing/agency/urls.py
@@ -4,5 +4,10 @@ from django.core.urlresolvers import reverse
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='index'),
+    url(r'^$', views.home_page, name='home'),
+    url(r'^add$', views.add_house, name='add'),
+    url(r'^post/(?P<pk>\d+)/$', views.house_detail, name='post'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.update_house, name='edit'),
+    url(r'^post/(?P<pk>\d+)/delete/$', views.delete_house, name='delete'),
+    
 ]
--- housing/agency/views.py
+++ housing/agency/views.py
@@ -1,23 +1,45 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Houses
+from .forms import add_houseForm
 
 # Create your views here.
 def home_page(request):
-    view = Houses.objects.all()
+    view = Houses.objects.order_by('-timestamp')
     context = {
         'view': view
     }
     return render(request, 'agency/index.html', context)
 
 def add_house(request):
-    return Httpresponse("<h1>create a listing</h1>")
+    form = add_houseForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+    	instance = form.save(commit=False)
+        print form.cleaned_data.get('name') 
+    	instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    return render (request, 'agency/new_post.html', {'form': form})
 
-def house_detail(request):
-    return Httpresponse("<h1>view details</h1>")
+def house_detail(request, pk):
+    view = get_object_or_404(Houses, pk=pk)
+    context = {
+        'view' : view
+    }
+    return render(request, 'agency/house_detail.html', context)
 
-def update_house(request):
-    return Httpresponse("<h1>update_house</h1>")
-
-def delete_house(request):
-    return Httpresponse("<h1>delete_house</h1>")
+def update_house(request, pk=None):
+    instance = get_object_or_404(Houses, pk=pk)
+    form = add_houseForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+        
+    context = {
+        'view': instance,
+        'form' : form,
+    }
+    return render(request, 'agency/new_post.html', context)
+def delete_house(request, pk=None):
+    instance = get_object_or_404(Houses, pk=pk)
+    instance.delete()
+    return redirect ('index.html')
