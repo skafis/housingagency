c35f62cd365de4053b1c5b7c6473ca0ad8eed4c2
skafis <franciskipchumba5@gmail.com>
2016-05-27 14:36:27 +0300

    fixed authentication problems

--- housing/housing/settings.py
+++ housing/housing/settings.py
@@ -12,7 +12,123 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
-from all_auth_settings import *
+# from all_auth_settings import *
+
+# django-allauth Configuration variables you might like to change.
+#
+# DO NOT CHANGE THIS FILE. Instead, copy it to local_settings.py
+# and make your changes there.
+
+
+# redirect to login
+LOGIN_URL = '/accounts/login/'
+# redirect to home
+LOGIN_REDIRECT_URL = '/'
+
+# Specifies the login method to use -- whether the user logs in by entering
+# their username, e-mail address, or either one of both. Possible values
+# are 'username' | 'email' | 'username_email'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+
+# The URL to redirect to after a successful e-mail confirmation, in case no
+# user is logged in. Default value is settings.LOGIN_URL.
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL
+
+# The URL to redirect to after a successful e-mail confirmation, in case of
+# an authenticated user. Default is settings.LOGIN_REDIRECT_URL
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL
+
+ACCOUNT_SESSION_REMEMBER =None
+# Determines the expiration date of email confirmation mails (# of days).
+# ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+
+# The user is required to hand over an e-mail address when signing up.
+ACCOUNT_EMAIL_REQUIRED = True
+
+# Determines the e-mail verification method during signup. When set to
+# "mandatory" the user is blocked from logging in until the email
+# address is verified. Choose "optional" or "none" to allow logins
+# with an unverified e-mail address. In case of "optional", the e-mail
+# verification mail is still sent, whereas in case of "none" no e-mail
+# verification mails are sent.
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
+# Subject-line prefix to use for email messages sent. By default, the name
+# of the current Site (django.contrib.sites) is used.
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Imax previews'
+
+# Logout without redirect
+ACCOUNT_LOGOUT_ON_GET = True
+
+# A string pointing to a custom form class
+# (e.g. 'myapp.forms.SignupForm') that is used during signup to ask
+# the user for additional input (e.g. newsletter signup, birth
+# date). This class should implement a `def signup(self, request, user)`
+# method, where user represents the newly signed up user.
+ACCOUNT_SIGNUP_FORM_CLASS = None
+
+# When signing up, let the user type in their password twice to avoid typ-o's.
+ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = True
+
+# Enforce uniqueness of e-mail addresses.
+ACCOUNT_UNIQUE_EMAIL = True
+
+# A callable (or string of the form 'some.module.callable_name') that takes
+# a user as its only argument and returns the display name of the user. The
+# default implementation returns user.username.
+# ACCOUNT_USER_DISPLAY
+
+# An integer specifying the minimum allowed length of a username.
+ACCOUNT_USERNAME_MIN_LENGTH = 3
+
+# The user is required to enter a username when signing up. Note that the
+# user will be asked to do so even if ACCOUNT_AUTHENTICATION_METHOD is set
+# to email. Set to False when you do not wish to prompt the user to enter a
+# username.
+ACCOUNT_USERNAME_REQUIRED = True
+
+# render_value parameter as passed to PasswordInput fields.
+ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = False
+
+# An integer specifying the minimum password length.
+ACCOUNT_PASSWORD_MIN_LENGTH = 6
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+
+# Request e-mail address from 3rd party account provider? E.g. using OpenID
+# AX, or the Facebook 'email' permission.
+SOCIALACCOUNT_QUERY_EMAIL = ACCOUNT_EMAIL_REQUIRED
+
+# Attempt to bypass the signup form by using fields (e.g. username, email)
+# retrieved from the social account provider. If a conflict arises due to a
+# duplicate e-mail address the signup form will still kick in.
+SOCIALACCOUNT_AUTO_SIGNUP = True
+
+# Enable support for django-avatar. When enabled, the profile image of the
+# user is copied locally into django-avatar at signup. Default is
+# 'avatar' in settings.INSTALLED_APPS.
+SOCIALACCOUNT_AVATAR_SUPPORT = True
+
+# Dictionary containing provider specific settings.
+# SOCIALACCOUNT_PROVIDERS ={ 
+#     'google':{
+#         'SCOPE': ['profile', 'email'],
+#         'AUTH_PARAMS': {
+#             'access_type': 'online' 
+#             } 
+#         }
+#     }
+
+
+SITE_ID=1
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
